410556129987b6ec08387a081d12903b3e563b59
william <william.mccumstie@outlook.com>
2018-07-14 23:52:51 +0100

    Move the library into the scripts dir
    The `lib` dir is on the require path for `FlightDirect` and should avoid
    getting polluted. Instead, the `scripts` directory is being used instead.
    
    The render path has also been adjusted to be within the scripts dir as
    well

---
+++ flight-direct/flight-completion/scripts/completion.rb
@@ -0,0 +1,17 @@
+#
+# Ruby Script for rendering FlightDirect bash completions
+#
+
+require_relative "#{ENV['FL_ROOT']}/lib/flight_direct.rb"
+require 'flight_direct/cli'
+
+return unless Process.euid == 0
+
+template = File.read(
+  File.join(FlightDirect.root_dir, 'scripts/bash_completion.sh.erb')
+)
+context = FlightDirect::CLI.instance_eval { binding }
+completion = ERB.new(template).result(context)
+file = File.join(FlightDirect.root_dir, 'scripts/bash_completion.sh')
+File.write(file, completion)
+
