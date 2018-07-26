0fd6e764e8b145a45ece4d49a77d6b577812099c
william <william.mccumstie@outlook.com>
2018-07-26 16:29:59 +0100

    Hide the sync add command on non-cache nodes
    The add command will only be displayed if the `public-dir` env var has
    been set. This means it will be hidden by default on nodes

--- flight-direct/flight-syncer/libexec/thor/sync.rb
+++ flight-direct/flight-syncer/libexec/thor/sync.rb
@@ -1,24 +1,29 @@
 #: SYNOPSIS: Sync files from the cache
 
 require 'flight_syncer'
+require 'flight_config'
 
-desc 'add FILE', 'Add a file to the local cache'
-METAFILE_PARAMS = {
-  path: 'The sync path of the file',
-  identifier: 'A unique refernce to the file, defaults to the basename',
-  mode: 'Sets the file permissions as an octal',
-  owner: 'Sets the files owner',
-  group: 'Sets the files group'
-}.each do |key, option_desc|
-  method_option key, desc: option_desc
-end
-def add(path)
-  content = File.read(path)
-  params = options.symbolize_keys.select do |k, _v|
-    METAFILE_PARAMS.keys.include?(k)
+# Only show the add command if the public static folder has been defined
+# This means it will not appear on regular nodes
+if FlightConfig.get('public-dir')
+  desc 'add FILE', 'Add a file to the local cache'
+  METAFILE_PARAMS = {
+    path: 'The sync path of the file',
+    identifier: 'A unique refernce to the file, defaults to the basename',
+    mode: 'Sets the file permissions as an octal',
+    owner: 'Sets the files owner',
+    group: 'Sets the files group'
+  }.each do |key, option_desc|
+    method_option key, desc: option_desc
   end
-  FlightSyncer::MetaFile.build_from_file(path, **params)
-                        .save_to_cache(content)
+  def add(path)
+    content = File.read(path)
+    params = options.symbolize_keys.select do |k, _v|
+      METAFILE_PARAMS.keys.include?(k)
+    end
+    FlightSyncer::MetaFile.build_from_file(path, **params)
+                          .save_to_cache(content)
+  end
 end
 
 desc 'file IDENTIFIER...', 'Sync a file from the cache'
