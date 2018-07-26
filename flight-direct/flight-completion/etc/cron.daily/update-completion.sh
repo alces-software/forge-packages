7d8f7e152f7597ec12af174cee62c55d7422fec5
william <william.mccumstie@outlook.com>
2018-07-26 16:50:58 +0100

    Move sourcing of the completion back to profile

--- flight-direct/flight-completion/etc/cron.daily/update-completion.sh
+++ flight-direct/flight-completion/etc/cron.daily/update-completion.sh
@@ -3,7 +3,4 @@
 #
 
 flight ruby "$FL_ROOT"/scripts/completion.rb
-if [[ -f "$FL_ROOT"/scripts/bash_completion.sh ]]; then
-  source "$FL_ROOT"/scripts/bash_completion.sh
-fi
 
---
+++ flight-direct/flight-completion/etc/profile.d/bash-completion.sh
@@ -0,0 +1,8 @@
+#
+# Sources the completion script (if it exists)
+#
+
+if [[ -f "$FL_ROOT"/scripts/bash_completion.sh ]]; then
+  source "$FL_ROOT"/scripts/bash_completion.sh
+fi
+
