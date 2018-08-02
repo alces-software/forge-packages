3f830a984f4537c1d087b2501f615dcd60eaf07d
william <william.mccumstie@outlook.com>
2018-08-02 16:47:38 +0100

    Group the cache commands together
    Previously the two add file and add group commands where implemented
    separately. This causes some code duplication and reduced the
    cohesiveness of the code.
    
    Instead they have been grouped together in a new `cache` command

--- flight-direct/flight-syncer/libexec/thor/sync.rb
+++ flight-direct/flight-syncer/libexec/thor/sync.rb
@@ -3,31 +3,40 @@
 require 'flight_syncer'
 require 'flight_config'
 
+# The cache commands are used to modify the sync manifest on the server
+class Cache < Thor
+  desc 'file PATH', 'Add a file to the local cache'
+  METAFILE_PARAMS = {
+    path: 'The sync path of the file',
+    identifier: 'A unique refernce to the file, defaults to the basename',
+    mode: 'Sets the file permissions as an octal',
+    owner: 'Sets the files owner',
+    group: 'Sets the files group'
+  }.each do |key, option_desc|
+    method_option key, desc: option_desc
+  end
+  def file(path)
+    content = File.read(path)
+    params = options.symbolize_keys.select do |k, _v|
+      METAFILE_PARAMS.keys.include?(k)
+    end
+    FlightSyncer::MetaFile.build_from_file(path, **params)
+                          .save_to_cache(content)
+  end
 
+  desc 'group NAME FILES...', 'Add files to a group'
+  def group(group, *files)
+    FlightSyncer::SyncManifest.update do |manifest|
+      manifest.add_files_to_group(group, *files)
+    end
+  end
+end
 
-
-# Only show the add command if the public static folder has been defined
-# This means it will not appear on regular nodes
-# if FlightConfig.get('public-dir')
-#   desc 'add FILE', 'Add a file to the local cache'
-#   METAFILE_PARAMS = {
-#     path: 'The sync path of the file',
-#     identifier: 'A unique refernce to the file, defaults to the basename',
-#     mode: 'Sets the file permissions as an octal',
-#     owner: 'Sets the files owner',
-#     group: 'Sets the files group'
-#   }.each do |key, option_desc|
-#     method_option key, desc: option_desc
-#   end
-#   def add(path)
-#     content = File.read(path)
-#     params = options.symbolize_keys.select do |k, _v|
-#       METAFILE_PARAMS.keys.include?(k)
-#     end
-#     FlightSyncer::MetaFile.build_from_file(path, **params)
-#                           .save_to_cache(content)
-#   end
-# end
+# Only show the cache commands if the public static folder has been defined
+if FlightConfig.get('public-dir')
+  desc 'cache SUBCOMMAND ...ARGS', 'Manage the sync server cache'
+  subcommand 'cache', Cache
+end
 
 # desc 'file IDENTIFIER...', 'Sync a file from the cache'
 # def file(*identifiers)
@@ -47,14 +56,6 @@ require 'flight_config'
 # class Group < Thor
 #   # Only show the 'add' command if on the cache server (aka the public dir
 #   # has been defined)
-#   if FlightConfig.get('public-dir')
-#     desc 'add GROUP FILES...', 'Add files to a group'
-#     def add(group, *files)
-#       FlightSyncer::SyncManifest.update do |manifest|
-#         manifest.add_files_to_group(group, *files)
-#       end
-#     end
-#   end
 
 #   desc 'get GROUP', 'Sync all the files within the group'
 #   def get(group)
