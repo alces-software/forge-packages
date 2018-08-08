ddf1881ce22b258a2074e5fa10cbaf8c2079efa7
william <william.mccumstie@outlook.com>
2018-08-08 11:38:18 +0100

    Update clusterware-storage to install offline
    It will now check the cache server for the tarball first

--- clusterware/clusterware-storage/package/install.sh
+++ clusterware/clusterware-storage/package/install.sh
@@ -5,6 +5,14 @@ cp -R data/* "${cw_ROOT}"
 rm -f "${cw_ROOT}"/etc/storage/.gitkeep
 mkdir -p "${cw_ROOT}"/opt/clusterware-storage
 
+# Tries to download the tarball from the cache server
+if [ -n "$FL_CONFIG_CACHE_URL" ]; then
+  url="${FL_CONFIG_CACHE_URL}/git/clusterware-storage.tar.gz"
+  pushd /tmp >/dev/null
+    wget $url 2>/dev/null
+  popd >/dev/null
+fi
+
 echo "Setting up storage base repository"
 if [ -d "${cw_ROOT}/var/lib/storage/repos" ]; then
     echo 'Detected existing repository.'
