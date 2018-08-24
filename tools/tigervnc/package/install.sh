ba4290e9939d9c26625da72892557a6dabd248e6
william <william.mccumstie@outlook.com>
2018-08-24 11:35:40 +0100

    Install `gnutls` with tigervnc
    The `gnutls` libraries where previously installed with clusterware. This
    now needs to be done by the package that requires it.

--- tools/tigervnc/package/install.sh
+++ tools/tigervnc/package/install.sh
@@ -3,7 +3,7 @@ require files
 files_load_config distro
 
 if [[ "$cw_DIST" == "el6" || "$cw_DIST" == "el7" ]]; then
-  yum install -y -e0 mesa-libGL libXdmcp pixman xorg-x11-fonts-misc
+  yum install -y -e0 mesa-libGL libXdmcp pixman xorg-x11-fonts-misc gnutls
   yum -e0 -y install uuid netpbm-progs iproute xauth xkeyboard-config xorg-x11-xkb-utils xorg-x11-apps xorg-x11-server-utils xterm
 elif [[ "$cw_DIST" == "ubuntu1604" ]]; then
   apt-get install -y libgl1-mesa-glx libglapi-mesa libxdmcp6 libpixman-1-0 xfonts-base x11-xserver-utils libjpeg8
