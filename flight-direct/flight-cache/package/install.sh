ca23dd701e5496d63414a1442061098ba4eb9511
william <william.mccumstie@outlook.com>
2018-10-08 15:22:42 +0100

    Port the install script from anvil
    The purpose of the `flight-cache` package is to remove the flight direct
    specific code out of the `anvil` repo. This install script is broadly
    based on:
    https://raw.githubusercontent.com/alces-software/anvil/394a8a6397305c5619c2cb2f5b50c213b477b3d0/scripts/install.sh
    
    It does not however preform the snapshot itself as this requires a user
    input. It does however get the service ready

--- flight-direct/flight-cache/package/install.sh
+++ flight-direct/flight-cache/package/install.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
 
+scripts_dir=$FL_ROOT/opt/anvil/scripts
 
+# Installs the required packages
+yum -y -e0 install gcc
 
+# Installs postgres if it is missing
+if ! which postgres 2>&1 >/dev/null; then
+  $scripts_dir/setup-postgres.sh
+fi
+
+# Ensures the profile has been sourced
+source ~/.bashrc
+
+# Sets the rails environment to be snapshot
+if ! [[ -z "RAILS_ENV" ]]; then
+  echo "export RAILS_ENV=snapshot" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+# Installs the gems
+cd $scripts_dir/..
+bundle install --without development --with default snapshot
+
+# Sets up systemd integration for anvil
+systemd=/usr/lib/systemd/system/anvil.service
+cat << SYSTEMD > $systemd
+[Unit]
+Description=Runs the anvil cache server
+Requires=network.target
+Requires=postgresql.service
+[Service]
+Type=simple
+ExecStart=/bin/bash $scripts_dir/start-anvil.sh
+TimeoutSec=30
+[Install]
+WantedBy=multi-user.target
+SYSTEMD
+
+# Applies the systemd change
+systemctl daemon-reload
