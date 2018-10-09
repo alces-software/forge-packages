919fecc5b5649020988ee4ad777a823c0ef794df
william <william.mccumstie@outlook.com>
2018-10-09 12:14:34 +0100

    Add a command to preform the package snapshot
    The `anvil snapshot` command will now run the snapshot. It `exec` into
    the command so that the user continues to get the output to stdout and
    stderr.
    
    This replaces the question on which address to build the package on
    
    NOTE: The `rake` command is still executed due to issues with requiring
    the rails app

--- flight-direct/flight-cache/data/fl_root/libexec/thor/anvil.rb
+++ flight-direct/flight-cache/data/fl_root/libexec/thor/anvil.rb
@@ -7,4 +7,7 @@ desc 'snapshot ADDRESS', 'Preform a package snapshot'
 long_desc <<-LONGDESC
 LONGDESC
 loki_command(:snapshot) do |address|
+  # Set the `BASE_URL` of the snapshot
+  ENV['ANVIL_BASE_URL'] = "http://#{address}"
+  exec("cd #{FlightDirect.root_dir}/opt/anvil && rake packages:snapshot")
 end
