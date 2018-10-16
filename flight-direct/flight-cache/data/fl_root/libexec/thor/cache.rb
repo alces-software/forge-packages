0e5839aa78dbd475e42de87ea0a69fd5da21902f
william <william.mccumstie@outlook.com>
2018-10-16 16:24:17 +0100

    Port the code for downloading the bootstrap file

--- flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
+++ flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
@@ -3,10 +3,27 @@
 
 require 'flight_config'
 require 'thor/group'
+require 'open-uri'
 
 class Snapshot < Thor::Group
   # NOTE: The following methods are ran in the order they have been defined
   # Some of the tasks are order dependent (e.g. download/import)
+  def download_flight_direct_bootstrap
+    puts 'Downloading FlightDirect bootstrap'
+    bootstrap_url = 'https://raw.githubusercontent.com/alces-software/flight-direct/master/scripts/bootstrap.sh'
+    bootstrap_path = File.join(ENV['ANVIL_LOCAL_DIR'],
+                               'flight-direct',
+                               'bootstrap.sh')
+    download(bootstrap_url, bootstrap_path)
+
+    bootstrap_content = File.read(bootstrap_path)
+    new_bootstrap_content = bootstrap_content.gsub(
+      /# anvil_url=/, "anvil_url=#{ENV['ANVIL_BASE_URL']}"
+    )
+    FileUtils.rm(bootstrap_path)
+    File.write(bootstrap_path, new_bootstrap_content)
+  end
+
   def database_setup
     run_rake('db:setup')
   end
@@ -26,6 +43,14 @@ class Snapshot < Thor::Group
   end
 
   private
+
+  def download(url, path)
+    FileUtils.mkdir_p File.dirname(path)
+    case io = open(url)
+    when StringIO then File.open(path, 'w') { |f| f.write(io.read) }
+    when Tempfile then FileUtils.mv(io.path, path)
+    end
+  end
 
   def run_rake(command)
     dir = File.join(FlightDirect.root_dir, 'opt/anvil')
