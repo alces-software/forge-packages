1ef8bc1eeac98b4b8794887647207cae084fb5ad
william <william.mccumstie@outlook.com>
2018-10-16 16:15:43 +0100

    Manually preform the snapshot steps
    A lot of the snapshot code will be moved into this file (eventually). To
    facilitate this, the snapshot has been broken down into steps. Some of
    these will remain rake tasks, others may change
    
    NOTE: Some of the snapshot code is only defined within the
    `packages:snapshot` task. As this task is going to be deprecated, this
    code is currently being skipped.

--- flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
+++ flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
@@ -5,13 +5,31 @@ require 'flight_config'
 require 'thor/group'
 
 class Snapshot < Thor::Group
-  def run_snapshot
-    dir = File.join(FlightDirect.root_dir, 'opt/anvil')
-    exec("cd #{dir} && rake packages:snapshot")
+  # NOTE: The following methods are ran in the order they have been defined
+  # Some of the tasks are order dependent (e.g. download/import)
+  def database_setup
+    run_rake('db:setup')
+  end
+
+  def download_packages
+    puts 'Downloading packages...'
+    run_rake('packages:download')
+  end
+
+  def import_packages
+    puts 'Importing the packages...'
+    run_rake('packages:import')
   end
 
   def enable_systemctl
-    exec('systemctl enable anvil')
+    system('systemctl enable anvil')
+  end
+
+  private
+
+  def run_rake(command)
+    dir = File.join(FlightDirect.root_dir, 'opt/anvil')
+    system("cd #{dir} && rake #{command}")
   end
 end
 
@@ -19,8 +37,11 @@ desc 'snapshot ADDRESS', 'Preform a package snapshot'
 long_desc <<-LONGDESC
 LONGDESC
 loki_command(:snapshot) do |address|
-  ENV['ANVIL_BASE_URL'] = "http://#{address}"
   ENV['RAILS_ENV'] = 'snapshot'
+  ENV['ANVIL_BASE_URL'] = "http://#{address}"
+  ENV['ANVIL_UPSTREAM'] = 'https://forge-api.alces-flight.com'
+  ENV['ANVIL_LOCAL_DIR'] = File.expand_path('opt/anvil/public',
+                                            FlightDirect.root_dir)
   Snapshot.start
 end
 
