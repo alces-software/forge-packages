eb113981e1e8074ddb705a892931bb36222e2397
william <william.mccumstie@outlook.com>
2018-10-16 15:54:07 +0100

    Use a Thor command group for the snapshot
    Thor supports running multiple commands as a group. Eventually the
    snapshot code will be moved into different commands within this group

--- flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
+++ flight-direct/flight-cache/data/fl_root/libexec/thor/cache.rb
@@ -2,19 +2,25 @@
 #: ROOT: true
 
 require 'flight_config'
+require 'thor/group'
+
+class Snapshot < Thor::Group
+  def run_snapshot
+    dir = File.join(FlightDirect.root_dir, 'opt/anvil')
+    exec("cd #{dir} && rake packages:snapshot")
+  end
+
+  def enable_systemctl
+    exec('systemctl enable anvil')
+  end
+end
 
 desc 'snapshot ADDRESS', 'Preform a package snapshot'
 long_desc <<-LONGDESC
 LONGDESC
 loki_command(:snapshot) do |address|
-  # Set the `BASE_URL` of the snapshot
   ENV['ANVIL_BASE_URL'] = "http://#{address}"
   ENV['RAILS_ENV'] = 'snapshot'
-  exec(<<~EOF
-cd #{FlightDirect.root_dir}/opt/anvil && \
-rake packages:snapshot && \
-systemctl enable anvil
-EOF
-)
+  Snapshot.start
 end
 
