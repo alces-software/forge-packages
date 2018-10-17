648388c05a68ae11a60d2288cb18683578de6489
william <william.mccumstie@outlook.com>
2018-10-17 17:41:11 +0100

    Bundle flight-cache with the parallel gem
    This will be used to port the download code from `anvil`

--- flight-direct/flight-cache/package/build.sh
+++ flight-direct/flight-cache/package/build.sh
@@ -14,6 +14,11 @@ temp_dir=$(mktemp -d /tmp/${package_name}-build-XXXXX)
 cp -r * "${temp_dir}"
 cp -r ../data/* "${temp_dir}"
 
+# Install the gems
+pushd "${temp_dir}/fl_root/opt/flight-cache" > /dev/null
+bundle install --path vendor/bundle
+popd > /dev/null
+
 pushd "${temp_dir}" > /dev/null
 zip -r ${package_name}.zip *
 popd > /dev/null
