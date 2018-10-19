490bc5fb88ef00411dc6cbce0b5a71bd2677bc1a
william <william.mccumstie@outlook.com>
2018-10-19 16:26:09 +0100

    Use the postgres package instead of the S3 tarball
    Now there is a specific postgres package for flight direct, the old
    tarball is now obsolete. Instead, the rails app should connect to the
    internal version.
    
    This does require compiling the `pg` gem with this particular version.
    As this can only be done at install time, a two step process is
    required.
    
    At build time, the gems are cached ('bundle package') and `pg` is
    configured to use the flight-direct posgres.
    
    Then at install time, the cache is used to compile all the gems. As
    postgres is a package dependency, it has already been installed. This
    allows `pg` to be built correctly

--- flight-direct/flight-cache/package/build.sh
+++ flight-direct/flight-cache/package/build.sh
@@ -5,8 +5,6 @@ set -e
 package_name='flight-cache'
 anvil_tag='0.2.0'
 
-yum install -y -e0 postgresql-devel wget
-
 if [ -f ./${package_name}.zip ]; then
   echo "Replacing existing ${package_name}.zip in this directory"
   rm ./${package_name}.zip
@@ -25,32 +23,17 @@ pushd "${temp_dir}/fl_root/opt" > /dev/null
 git clone https://github.com/alces-software/anvil.git
 cd anvil
 git checkout $anvil_tag
+
+# Remove the .git directory
+rm -rf .git
+
+bundle config build.pg --with-pg-config=$FL_ROOT/opt/postgres/bin/pg_config
 bundle package
 pushd > /dev/null
 
 # Installs the flight-cache gems
 pushd "${temp_dir}/fl_root/opt/flight-cache" > /dev/null
 bundle install --path vendor/bundle
-popd > /dev/null
-
-# Pulls in the compiled version of postgres
-pushd $temp_dir > /dev/null
-#
-# The commented code was used to create the tarball, It is being kept as a
-# future reference
-#
-# Fetches the postgres source code
-# postgres='postgresql-9.6.9'
-# wget https://ftp.postgresql.org/pub/source/v9.6.9/$postgres.tar.gz
-# tar -xzf $postgres.tar.gz
-# cd $postgres
-#
-# Configures and installs postgres
-# ./configure --prefix=/usr
-# make world -j $(nproc)
-# make install-world -j $(nproc)
-#
-wget https://s3-eu-west-1.amazonaws.com/flight-direct/extract-postgres.sh
 popd > /dev/null
 
 pushd "${temp_dir}" > /dev/null
--- flight-direct/flight-cache/package/install.sh
+++ flight-direct/flight-cache/package/install.sh
@@ -3,10 +3,9 @@
 # Moves the directories in place
 cp -r ./fl_root/* $FL_ROOT
 
-# Installs postgres if it is missing
-if ! which postgres 2>&1 >/dev/null; then
-  ./setup-postgres.sh
-fi
+# Install the anvil gems
+cd $FL_ROOT/opt/anvil
+bundle install --with snapshot default --without development
 
 # Sets up systemd integration for anvil
 systemd=/usr/lib/systemd/system/flight-cache.service
@@ -14,7 +13,7 @@ cat << SYSTEMD > $systemd
 [Unit]
 Description=Runs the anvil cache server
 Requires=network.target
-Requires=postgresql.service
+Requires=postgres-flight.service
 [Service]
 Type=simple
 ExecStart=/bin/bash $FL_ROOT/opt/flight-cache/scripts/start-anvil.sh
