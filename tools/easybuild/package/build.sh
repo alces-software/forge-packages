e72836e9f7a22f875c5674cc758315d52de42292
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-10-31 17:22:51 +0000

    Bump easybuild to v3.7.1

--- tools/easybuild/package/build.sh
+++ tools/easybuild/package/build.sh
@@ -14,22 +14,22 @@ cp -r * "${temp_dir}"
 pushd "$temp_dir/data" > /dev/null
 mkdir "${temp_dir}/src"
 
-curl -L -o "${temp_dir}/src/vsc-base-2.5.8.tar.gz" https://files.pythonhosted.org/packages/f7/66/1ff7ecc4a93ba37e063f5bfbe395e95a547b1dec73b017c2724f4475a958/vsc-base-2.5.8.tar.gz
-curl -L -o "${temp_dir}/src/vsc-install-0.10.32.tar.gz" https://files.pythonhosted.org/packages/5d/ca/1c41be2964be1355e15b4a88c7eef11c13c621220e27b69b2686c23cace2/vsc-install-0.10.32.tar.gz
-curl -L -o "${temp_dir}/src/easybuild-framework-3.5.3.tar.gz" https://files.pythonhosted.org/packages/6f/a2/0c364993ee5264415e2b46af6323bbdcab610d36ceaca39c253c69cf40a1/easybuild-framework-3.5.3.tar.gz
-curl -L -o "${temp_dir}/src/easybuild-easyblocks-3.5.3.tar.gz" https://files.pythonhosted.org/packages/3f/9a/cd137add36144a67368c8b472ec91b3475f95cbaf89442a394a5fe77dc53/easybuild-easyblocks-3.5.3.tar.gz
-curl -L -o "${temp_dir}/src/easybuild-easyconfigs-3.5.3.tar.gz" https://files.pythonhosted.org/packages/86/c2/b6ddd15854148d6b2396f402526c64ea017041035e854cafd62f8b5c6a60/easybuild-easyconfigs-3.5.3.tar.gz
+curl -L -o "${temp_dir}/src/vsc-base-2.8.3.tar.gz" https://files.pythonhosted.org/packages/62/e5/589612e47255627e4752d99018ae7cff8f49ab0fa6b4ba7b2226a76a05d3/vsc-base-2.8.3.tar.gz
+curl -L -o "${temp_dir}/src/vsc-install-0.11.2.tar.gz" https://files.pythonhosted.org/packages/b6/03/becd813f5c4e8890254c79db8d2558b658f5a3ab52157bc0c077c6c9beea/vsc-install-0.11.2.tar.gz
+curl -L -o "${temp_dir}/src/easybuild-framework-3.7.1.tar.gz" https://files.pythonhosted.org/packages/d0/f1/a3c897ab19ad36a9a259adc0b31e383a8d322942eda1e59eb4fedee27d09/easybuild-framework-3.7.1.tar.gz
+curl -L -o "${temp_dir}/src/easybuild-easyblocks-3.7.1.tar.gz" https://files.pythonhosted.org/packages/50/ea/3381a6e85f9a9beee311bed81a03c4900dd11c2a25c1e952b76e9a73486b/easybuild-easyblocks-3.7.1.tar.gz
+curl -L -o "${temp_dir}/src/easybuild-easyconfigs-3.7.1.tar.gz" https://files.pythonhosted.org/packages/73/63/b22ff96b8c3e09e04466951c0c3aa7b2230a522792dd3ae37c5fce4c68ea/easybuild-easyconfigs-3.7.1.tar.gz
 curl -L -o "${temp_dir}/src/bootstrap_eb.py" https://github.com/easybuilders/easybuild-framework/raw/master/easybuild/scripts/bootstrap_eb.py
 
 export EASYBUILD_BOOTSTRAP_SOURCEPATH="${temp_dir}/src"
-PATH="/opt/clusterware/opt/modules/bin:$PATH"
-mkdir /opt/clusterware/opt/easybuild
-chown alces /opt/clusterware/opt/easybuild
+PATH="/opt/flight-direct/opt/modules/bin:$PATH"
+mkdir /opt/flight-direct/opt/easybuild
+chown centos /opt/flight-direct/opt/easybuild
 chmod 755 ${temp_dir}
-sudo -E PYTHONDONTWRITEBYTECODE=true -E PATH=$PATH -u alces python ${temp_dir}/src/bootstrap_eb.py /opt/clusterware/opt/easybuild
+sudo -E PYTHONDONTWRITEBYTECODE=true -E PATH=$PATH -u centos python ${temp_dir}/src/bootstrap_eb.py /opt/flight-direct/opt/easybuild
 
 mkdir opt
-mv /opt/clusterware/opt/easybuild opt
+mv /opt/flight-direct/opt/easybuild opt
 chown -R root:root opt/easybuild
 
 rm -rf "${temp_dir}"/src
