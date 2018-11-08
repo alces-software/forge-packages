f7a7a048abdfdf950350a14e95ec933e1ecbec02
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-11-08 12:15:08 +0000

    Update slurm-master for compatibility with Flight Direct v2

--- schedulers/slurm-master/package/install.sh
+++ schedulers/slurm-master/package/install.sh
@@ -8,11 +8,19 @@ if [[ "$cw_DIST" == "el6" ]]; then
 fi
 
 sed -e "s,_cw_ROOT_,${cw_ROOT},g" \
-    data/init/systemd/clusterware-slurm-slurmctld.service \
-    > /etc/systemd/system/clusterware-slurm-slurmctld.service
-cat <<EOF > /etc/tmpfiles.d/clusterware-slurm.conf
-# Clusterware Slurm runtime directory
+    data/init/systemd/flight-slurm-slurmctld.service \
+    > /etc/systemd/system/flight-slurm-slurmctld.service
+cat <<EOF > /etc/tmpfiles.d/flight-slurm.conf
+# Flight Slurm runtime directory
 d /run/slurm 0755 slurm root -
 EOF
 
-systemctl enable clusterware-slurm-slurmctld.service
+systemctl enable flight-slurm-slurmctld.service
+
+# Create needed system dirs owned by Slurm user.
+slurm_system_dirs=(/var/{log,run,spool}/slurm)
+slurm_user='slurm'
+mkdir -p "${slurm_system_dirs[@]}"
+touch /var/log/slurm/accounting
+chown -R "${slurm_user}:${slurm_user}" "${slurm_system_dirs[@]}"
+chmod 0644 /var/log/slurm/accounting
